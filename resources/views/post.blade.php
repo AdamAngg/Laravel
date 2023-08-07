5ecedef5ed79626ba2586bc612cda71c7bef206a
AdamAngg <adam.nglart@gmail.com>
2023-08-07 23:58:20 +0200

    style: back btn in post

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -13,5 +13,6 @@
 </h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae beatae enim dignissimos aperiam eveniet. Repudiandae nulla alias, rerum beatae et iusto veritatis magnam praesentium. Voluptatum perferendis dolorum impedit itaque praesentium, veritatis fugit numquam aliquid reprehenderit quasi sequi suscipit porro quaerat aliquam magni modi amet incidunt repellat, rem saepe placeat libero nisi recusandae. Fugit eaque consectetur, delectus omnis qui assumenda necessitatibus minus sunt enim itaque quasi impedit, in modi voluptatibus totam soluta quidem dignissimos. Culpa porro incidunt dicta facilis, libero quia blanditiis iure ipsum impedit doloremque adipisci velit, animi dolores explicabo, id unde totam consectetur neque cupiditate reprehenderit et sapiente aliquid!</p>
    </article>
+   <a href="/">Go back</a>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('posts');
 });
-Route::get('/post', function() {
+Route::get('post', function() {
     return view('post');
 });
