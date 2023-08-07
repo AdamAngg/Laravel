cae064923cf781e956e0f6bd4af4faa8dd50540e
AdamAngg <adam.nglart@gmail.com>
2023-08-07 23:53:39 +0200

    fix: path problem

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -8,15 +8,24 @@
 </head>
 <body>
    <article>
-    <h1>My first posts</h1>
+    <h1><a href="post">
+        My 1 posts
+    </a> 
+</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae beatae enim dignissimos aperiam eveniet. Repudiandae nulla alias, rerum beatae et iusto veritatis magnam praesentium. Voluptatum perferendis dolorum impedit itaque praesentium, veritatis fugit numquam aliquid reprehenderit quasi sequi suscipit porro quaerat aliquam magni modi amet incidunt repellat, rem saepe placeat libero nisi recusandae. Fugit eaque consectetur, delectus omnis qui assumenda necessitatibus minus sunt enim itaque quasi impedit, in modi voluptatibus totam soluta quidem dignissimos. Culpa porro incidunt dicta facilis, libero quia blanditiis iure ipsum impedit doloremque adipisci velit, animi dolores explicabo, id unde totam consectetur neque cupiditate reprehenderit et sapiente aliquid!</p>
    </article>
    <article>
-    <h1>My second posts</h1>
+   <h1><a href="public/post">
+        My 2 posts
+    </a> 
+</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae beatae enim dignissimos aperiam eveniet. Repudiandae nulla alias, rerum beatae et iusto veritatis magnam praesentium. Voluptatum perferendis dolorum impedit itaque praesentium, veritatis fugit numquam aliquid reprehenderit quasi sequi suscipit porro quaerat aliquam magni modi amet incidunt repellat, rem saepe placeat libero nisi recusandae. Fugit eaque consectetur, delectus omnis qui assumenda necessitatibus minus sunt enim itaque quasi impedit, in modi voluptatibus totam soluta quidem dignissimos. Culpa porro incidunt dicta facilis, libero quia blanditiis iure ipsum impedit doloremque adipisci velit, animi dolores explicabo, id unde totam consectetur neque cupiditate reprehenderit et sapiente aliquid!</p>
    </article>
    <article>
-    <h1>My third posts</h1>
+   <h1><a href="public/post">
+        My 3 posts
+    </a> 
+</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae beatae enim dignissimos aperiam eveniet. Repudiandae nulla alias, rerum beatae et iusto veritatis magnam praesentium. Voluptatum perferendis dolorum impedit itaque praesentium, veritatis fugit numquam aliquid reprehenderit quasi sequi suscipit porro quaerat aliquam magni modi amet incidunt repellat, rem saepe placeat libero nisi recusandae. Fugit eaque consectetur, delectus omnis qui assumenda necessitatibus minus sunt enim itaque quasi impedit, in modi voluptatibus totam soluta quidem dignissimos. Culpa porro incidunt dicta facilis, libero quia blanditiis iure ipsum impedit doloremque adipisci velit, animi dolores explicabo, id unde totam consectetur neque cupiditate reprehenderit et sapiente aliquid!</p>
    </article>
 </body>
