b59f0204d59bd46b691055fed59d470bed6de6e1
AdamAngg <adam.nglart@gmail.com>
2023-08-07 22:12:07 +0200

    style: adding some style and routes

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="app.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My blog</title>
+</head>
+<body>
+   <article>
+    <h1>My first posts</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae beatae enim dignissimos aperiam eveniet. Repudiandae nulla alias, rerum beatae et iusto veritatis magnam praesentium. Voluptatum perferendis dolorum impedit itaque praesentium, veritatis fugit numquam aliquid reprehenderit quasi sequi suscipit porro quaerat aliquam magni modi amet incidunt repellat, rem saepe placeat libero nisi recusandae. Fugit eaque consectetur, delectus omnis qui assumenda necessitatibus minus sunt enim itaque quasi impedit, in modi voluptatibus totam soluta quidem dignissimos. Culpa porro incidunt dicta facilis, libero quia blanditiis iure ipsum impedit doloremque adipisci velit, animi dolores explicabo, id unde totam consectetur neque cupiditate reprehenderit et sapiente aliquid!</p>
+   </article>
+   <article>
+    <h1>My second posts</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae beatae enim dignissimos aperiam eveniet. Repudiandae nulla alias, rerum beatae et iusto veritatis magnam praesentium. Voluptatum perferendis dolorum impedit itaque praesentium, veritatis fugit numquam aliquid reprehenderit quasi sequi suscipit porro quaerat aliquam magni modi amet incidunt repellat, rem saepe placeat libero nisi recusandae. Fugit eaque consectetur, delectus omnis qui assumenda necessitatibus minus sunt enim itaque quasi impedit, in modi voluptatibus totam soluta quidem dignissimos. Culpa porro incidunt dicta facilis, libero quia blanditiis iure ipsum impedit doloremque adipisci velit, animi dolores explicabo, id unde totam consectetur neque cupiditate reprehenderit et sapiente aliquid!</p>
+   </article>
+   <article>
+    <h1>My third posts</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae beatae enim dignissimos aperiam eveniet. Repudiandae nulla alias, rerum beatae et iusto veritatis magnam praesentium. Voluptatum perferendis dolorum impedit itaque praesentium, veritatis fugit numquam aliquid reprehenderit quasi sequi suscipit porro quaerat aliquam magni modi amet incidunt repellat, rem saepe placeat libero nisi recusandae. Fugit eaque consectetur, delectus omnis qui assumenda necessitatibus minus sunt enim itaque quasi impedit, in modi voluptatibus totam soluta quidem dignissimos. Culpa porro incidunt dicta facilis, libero quia blanditiis iure ipsum impedit doloremque adipisci velit, animi dolores explicabo, id unde totam consectetur neque cupiditate reprehenderit et sapiente aliquid!</p>
+   </article>
+</body>
+</html>
