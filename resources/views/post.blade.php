19990149002341a031ee870b3468d7b343f4758d
AdamAngg <adam.nglart@gmail.com>
2023-08-08 14:32:02 +0200

    basic routing & caching

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,14 +4,12 @@
     <link rel="stylesheet" href="app.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Post number X</title>
+    <title>Post number  <?= $slug;?></title>
 </head>
 <body>
    <article>
-   <h1>
-        My post X
-</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae beatae enim dignissimos aperiam eveniet. Repudiandae nulla alias, rerum beatae et iusto veritatis magnam praesentium. Voluptatum perferendis dolorum impedit itaque praesentium, veritatis fugit numquam aliquid reprehenderit quasi sequi suscipit porro quaerat aliquam magni modi amet incidunt repellat, rem saepe placeat libero nisi recusandae. Fugit eaque consectetur, delectus omnis qui assumenda necessitatibus minus sunt enim itaque quasi impedit, in modi voluptatibus totam soluta quidem dignissimos. Culpa porro incidunt dicta facilis, libero quia blanditiis iure ipsum impedit doloremque adipisci velit, animi dolores explicabo, id unde totam consectetur neque cupiditate reprehenderit et sapiente aliquid!</p>
+   <?= $post;?>
+   
    </article>
    <a href="/">Go back</a>
 </body>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -8,21 +8,21 @@
 </head>
 <body>
    <article>
-    <h1><a href="post">
+    <h1><a href="posts/my-first-post">
         My 1 posts
     </a> 
 </h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae beatae enim dignissimos aperiam eveniet. Repudiandae nulla alias, rerum beatae et iusto veritatis magnam praesentium. Voluptatum perferendis dolorum impedit itaque praesentium, veritatis fugit numquam aliquid reprehenderit quasi sequi suscipit porro quaerat aliquam magni modi amet incidunt repellat, rem saepe placeat libero nisi recusandae. Fugit eaque consectetur, delectus omnis qui assumenda necessitatibus minus sunt enim itaque quasi impedit, in modi voluptatibus totam soluta quidem dignissimos. Culpa porro incidunt dicta facilis, libero quia blanditiis iure ipsum impedit doloremque adipisci velit, animi dolores explicabo, id unde totam consectetur neque cupiditate reprehenderit et sapiente aliquid!</p>
    </article>
    <article>
-   <h1><a href="public/post">
+   <h1><a href="posts/my-second-post">
         My 2 posts
     </a> 
 </h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae beatae enim dignissimos aperiam eveniet. Repudiandae nulla alias, rerum beatae et iusto veritatis magnam praesentium. Voluptatum perferendis dolorum impedit itaque praesentium, veritatis fugit numquam aliquid reprehenderit quasi sequi suscipit porro quaerat aliquam magni modi amet incidunt repellat, rem saepe placeat libero nisi recusandae. Fugit eaque consectetur, delectus omnis qui assumenda necessitatibus minus sunt enim itaque quasi impedit, in modi voluptatibus totam soluta quidem dignissimos. Culpa porro incidunt dicta facilis, libero quia blanditiis iure ipsum impedit doloremque adipisci velit, animi dolores explicabo, id unde totam consectetur neque cupiditate reprehenderit et sapiente aliquid!</p>
    </article>
    <article>
-   <h1><a href="public/post">
+   <h1><a href="posts/my-third-post">
         My 3 posts
     </a> 
 </h1>
