e2a88ce61bf6eee7f320bce7aef3cf36b3cd473d
AdamAngg <adam.nglart@gmail.com>
2023-08-09 20:05:08 +0200

    feat: blade layout

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,21 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <link rel="stylesheet" href="app.css">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Post number X</title>
-</head>
-<body>
-   <article>
+@extends('layout')
+
+@section('content')
+<article>
     <h1>{{$post->title}}</h1>
     <div>
-
-        {{strip_tags($post->body)}}
+        {!!$post->body!!}
     </div>
 
    
    </article>
    <a href="/">Go back</a>
-</body>
-</html>
+@endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,13 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <link rel="stylesheet" href="app.css">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My blog</title>
-</head>
-<body>
-    <?php foreach ($posts as $post) : ?>
+@extends ('layout')
+
+@section('content')
+ @foreach ($posts as $post)
         <article>
           <h1>
             <a href="posts/{{$post->slug}}"> {{$post->title}}</a>
@@ -16,6 +10,5 @@
           </p>
           {{$post->date}}       
         </article>
-     <?php  endforeach ?>
-    </body>
-    </html>
+     @endforeach
+@endsection
