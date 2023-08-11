8e9aadd9e71dca15d9ff9671c528bdf80d37ff3c
AdamAngg <adam.nglart@gmail.com>
2023-08-11 10:48:12 +0200

    feat:working with db seeds

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
 use Illuminate\Database\Seeder;
+use \App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,7 +15,44 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $user = User::factory()->create();
+
+        $family = Category::create([
+            'name'=> 'Family',
+            'slug'=> 'family'
+        ]);
+        $work = Category::create([
+            'name'=> 'Work',
+            'slug'=> 'work'
+        ]);
+        $hobby = Category::create([
+            'name'=> 'Hobby',
+            'slug'=> 'hobby'
+        ]);
+        Post::create([
+            'user_id' => $user->id,
+            'category_id'=> $family->id,
+            'title'=> 'My family post',
+            'slug' => 'my-first-post',
+            'excerpt' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas, voluptate.',
+            'body'=> 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio deserunt possimus reprehenderit natus temporibus rerum ut optio! Vel quisquam, minus ea eligendi, dicta molestiae consectetur labore, nesciunt recusandae fugiat a!'
+        ]);
+        Post::create([
+            'user_id' => $user->id,
+            'category_id'=> $family->id,
+            'title'=> 'My family post',
+            'slug' => 'my-first-post',
+            'excerpt' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas, voluptate.',
+            'body'=> 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio deserunt possimus reprehenderit natus temporibus rerum ut optio! Vel quisquam, minus ea eligendi, dicta molestiae consectetur labore, nesciunt recusandae fugiat a!'
+        ]);
+        Post::create([
+            'user_id' => $user->id,
+            'category_id'=> $family->id,
+            'title'=> 'My family post',
+            'slug' => 'my-first-post',
+            'excerpt' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas, voluptate.',
+            'body'=> 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio deserunt possimus reprehenderit natus temporibus rerum ut optio! Vel quisquam, minus ea eligendi, dicta molestiae consectetur labore, nesciunt recusandae fugiat a!'
+        ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -6,7 +6,7 @@
     <div>
         {!!$post->body!!}
     </div> 
-    <p><a href="category/{{$post->category->slug}}">{{$post->category->name}}</a></p>
+    <p>By <a href="#">Je je </a><a href="category/{{$post->category->slug}}">{{$post->category->name}}</a></p>
    </article>
    <a href="/">Go back</a>
 @endsection
