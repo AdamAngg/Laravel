04d9713b358fc55c4737d80a948db586aba7d53f
AdamAngg <adam.nglart@gmail.com>
2023-08-11 15:03:16 +0200

    style: converting html+css to blade

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,7 +1,29 @@
 @extends ('layout')
 
 @section('content')
- @foreach ($posts as $post)
+
+@include('post-header')
+
+        <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        
+        @include('post-featured')
+
+            <div class="lg:grid lg:grid-cols-2">
+
+            @include('post-card')
+            @include('post-card')
+
+            </div>
+            <div class="lg:grid lg:grid-cols-3">
+            
+            @include('post-card')
+            @include('post-card')
+            @include('post-card')
+           
+            
+            </div>
+        </main>
+ <!-- @foreach ($posts as $post)
         <article>
           <h1>
             <a href="posts/{{$post->slug}}"> {{$post->title}}</a>
@@ -10,5 +32,5 @@
           </p>
           <p>By <a href="author/{{$post->author->slug}}">{{$post->author->name}} </a> in <a href="category/{{$post->category->slug}}">{{$post->category->name}}</a></p>
         </article>
-     @endforeach
+     @endforeach -->
 @endsection
