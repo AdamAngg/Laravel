c1e756425a921fe4b0648d8fd0cb4d838b69510c
AdamAngg <adam.nglart@gmail.com>
2023-08-11 20:03:03 +0200

    feat:now using blade components

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,36 +1,42 @@
-@extends ('layout')
+<x-layout>
 
-@section('content')
-
-@include('post-header')
-
-        <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        
-        @include('post-featured')
-
-            <div class="lg:grid lg:grid-cols-2">
-
-            @include('post-card')
-            @include('post-card')
-
+  <x-post-header/>
+  
+  <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+    
+         <x-post-featured :post="$posts[0]"/>
+          
+          <div class="lg:grid lg:grid-cols-2">
+            
+            <x-post-card/>
+            <x-post-card/>
+            
             </div>
             <div class="lg:grid lg:grid-cols-3">
+                            
+            <x-post-card/>
+            <x-post-card/>
+            <x-post-card/>              
+              
+              </div>
+            </main>
             
-            @include('post-card')
-            @include('post-card')
-            @include('post-card')
-           
-            
-            </div>
-        </main>
- <!-- @foreach ($posts as $post)
+</x-layout>
+     
+
+
+
+
+
+
+
+       <!-- @foreach ($posts as $post)
         <article>
           <h1>
             <a href="posts/{{$post->slug}}"> {{$post->title}}</a>
           </h1><p>
-           {!!$post->excerpt!!}
+            {!!$post->excerpt!!}
           </p>
           <p>By <a href="author/{{$post->author->slug}}">{{$post->author->name}} </a> in <a href="category/{{$post->category->slug}}">{{$post->category->name}}</a></p>
         </article>
-     @endforeach -->
-@endsection
+        @endforeach -->
